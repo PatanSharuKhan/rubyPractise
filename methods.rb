fb636512de185cf69c5edeba90bd3d1ba10e1d02
patansharukhan <patansharu654321@gmail.com>
2023-07-12 00:38:58 +0530

    more modules added

--- methods.rb
+++ methods.rb
@@ -28,4 +28,57 @@ module Utility
         mn=b if b<a
         return mn
     end
+
+    def Utility.sort(arr,type)
+        case type
+        when "ASC"
+            for _ in (0..arr.length)
+                for i in (0...arr.length-1)
+                    if arr[i]>arr[i+1]
+                        arr[i],arr[i+1]=arr[i+1],arr[i]
+                    end
+                end
+            end
+            return arr
+        when "DESC"
+            for _ in (0..arr.length)
+                for i in (0...arr.length-1)
+                    if arr[i]<arr[i+1]
+                        arr[i],arr[i+1]=arr[i+1],arr[i]
+                    end
+                end
+            end
+            return arr
+        end
+    end
+
+    def Utility.evens(a,b)
+        lst=[]
+        for each in (a..b)
+            if each%2==0
+                lst.push(each)
+            end
+        end
+        return lst
+    end
+
+    def Utility.odds(a,b)
+        lst=[]
+        for each in (a..b)
+            if each%2!=0
+                lst.push(each)
+            end
+        end
+        return lst
+    end
+
+    def Utility.fact(num)
+        if num==1
+            return 1
+        end
+        return num*fact(num-1)
+    end
+
+    def Utility.getVowels(word)
+    end
 end
--- practise.rb
+++ practise.rb
@@ -22,3 +22,32 @@ puts Utility.max 10,20
 
 # min number 
 puts Utility.min 10,20
+
+# sort array
+print Utility.sort [3,2,1,5,6,7],"ASC"
+puts
+print Utility.sort [3,2,1,5,6,7],"DESC"
+puts
+
+# print even numbers
+print Utility.evens 1,100
+puts 
+
+# print odd numbers
+print Utility.odds 1,100
+puts
+
+# factorial of number
+puts Utility.fact 5
+
+# vowels in string
+puts Utility.getVowels "sharukhan"
+
+# consonents in string
+puts Utility.getConsonents "sharukhan"
+
+# alphabets count
+puts Utility.alphaCount "sharukhan patan"
+
+# remove spaces 
+puts Utility.removeSpaces "sharukhan patan"
