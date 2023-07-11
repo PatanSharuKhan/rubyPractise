9b7bc872f5abcef45e4feb47c6e5544521b89eaf
patansharukhan <patansharu654321@gmail.com>
2023-07-11 21:59:48 +0530

    message

---
+++ practise.rb
@@ -0,0 +1,16 @@
+
+
+
+def createInvertedPyramid(rows)
+    begin
+        puts rows
+    rescue => exception
+        puts exception
+    ensure
+        puts "thank you"
+    end  
+end
+
+
+rows=2
+createInvertedPyramid rows
